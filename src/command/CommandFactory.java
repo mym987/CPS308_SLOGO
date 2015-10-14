c29119947c0f1fa59aeea81ca16552cd540ff6a9
Mike Ma <ym67@duke.edu>
2015-10-14 10:38:50 -0400

    Update parser

--- src/command/CommandFactory.java
+++ src/command/CommandFactory.java
@@ -1,6 +1,13 @@
 package command;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 import model.Actions;
+import parser.ParseFormatException;
+import util.PropertyLoader;
 /**
  * Must use the same instance to produce commands
  * Otherwise variable scope might not work 
@@ -11,12 +18,46 @@ import model.Actions;
 public class CommandFactory {
 	
 	private Actions myActions;
+	private Map<String,Integer> myNumArgsRules;
 	
-	public CommandFactory(Actions actions){
+	public CommandFactory(Actions actions) throws IOException{
 		myActions = actions;
+		myNumArgsRules = getArgumentsRules();
+	}
+	
+	private Map<String,Integer> getArgumentsRules() throws IOException{
+		Map<String,Integer> argsMap = new HashMap<>();
+		Properties prop = (new PropertyLoader()).load("Commands");
+		prop.forEach((k,v)->{
+			argsMap.put(k.toString(), Integer.parseInt(v.toString()));
+		});
+		return argsMap;
 	}
 	
 	public Command getCommand(String name,Command...args){
+		//TODO
 		return null;
+	}
+	
+	public Command getConstant(double value){
+		return (args)->{return value;};
+	}
+	
+	public Command getVarable(String name, double value){
+		//TODO
+		return null;
+	}
+	
+	public Command getUserFunction(String name){
+		//TODO
+		return null;
+	}
+	
+	public int getNumArgs(String name) throws ParseFormatException{
+		if(myNumArgsRules==null)
+			throw new ParseFormatException("Command factory has not been initialized!");
+		if(!myNumArgsRules.containsKey(name))
+			throw new ParseFormatException(name+" does not exist!");
+		return myNumArgsRules.get(name);
 	}
 }
--- src/parser/SimpleParser.java
+++ src/parser/SimpleParser.java
@@ -24,7 +24,6 @@ public class SimpleParser implements Parser {
 	private Set<String> myUserFunctions;
 	
 	private Map<String,String> myLanguageRules;
-	private Map<String,Integer> myNumArgsRules;
 	private Map<String,String> mySyntaxRules;
 	
 	private Stack<Token> myTokenStack;
@@ -53,11 +52,14 @@ public class SimpleParser implements Parser {
 		}
 	}
 	
-	public SimpleParser(Actions actions){
-		myFactory = new CommandFactory(actions);
-		myLoader = new LanguageLoader();
-		myUserFunctions = new HashSet<>();
-		new HashMap<>();
+	public SimpleParser(Actions actions) throws ParseFormatException {
+		try {
+			myFactory = new CommandFactory(actions);
+			myLoader = new LanguageLoader();
+			myUserFunctions = new HashSet<>();
+		} catch (Exception e) {
+			throw new ParseFormatException(e.getMessage());
+		}
 	}
 	
 	private <K, V> void printMap(Map<K,V> map){
@@ -80,31 +82,56 @@ public class SimpleParser implements Parser {
 		 * GroupStart = \\(
 		 * GroupEnd = \\)
 		 */
-		while(myTokenizer.hasNext()){
+		while (myTokenizer.hasNext()) {
 			String token = myTokenizer.next();
-			if(token.matches(mySyntaxRules.get("Command"))){
-				pushCommand(token);
-			}else if(token.matches(mySyntaxRules.get("Constant"))){
+			if (token.matches(mySyntaxRules.get("Command"))) {
+				token = commandDelocalize(token);
+				myTokenStack.push(new Token(token, myFactory.getNumArgs(token)));
+			} else if (token.matches(mySyntaxRules.get("Constant"))) {
 				parseConstant(token);
-			}else if(token.matches(mySyntaxRules.get("Command")))
-				
+			} else if (token.matches(mySyntaxRules.get("Variable"))) {
+				parseVariable(token);
+			} else if (token.matches(mySyntaxRules.get("ListStart"))){
+				myTokenStack.push(new Token("List",Integer.MAX_VALUE));
+			} else if (token.matches(mySyntaxRules.get("GroupStart"))){
+				myTokenStack.push(new Token("Group",Integer.MAX_VALUE));
+			} else if (token.matches(mySyntaxRules.get("ListEnd"))){
+				closeCluster("List");
+			} else if (token.matches(mySyntaxRules.get("GroupEnd"))){
+				closeCluster("Group");
+			}
+			while(myTokenStack.peek().satisfied()){
+				popStack();
+			}
 		}
 		myTokenizer.close();
 		return myCommandList;
 	}
 	
-	/**
-	 * Push the command to the top of the stack
-	 * @param token
-	 */
-	private void pushCommand(String token){
-		
+	private String commandDelocalize(String token){
+		//TODO
+		return null;
+	}
+	
+	private void popStack(){
+		//TODO
+	}
+	
+	private void closeCluster(String listName) throws ParseFormatException{
+		String str = myTokenStack.peek().myName;
+		if(!str.equals(listName))
+			throw new ParseFormatException(str + " missing arguments or "+listName+" mismatch!");
+		popStack();
+	}
+	
+	private void parseVariable(String token) throws ParseFormatException{
+		//TODO
 	}
 	
 	private void parseConstant(String token) throws ParseFormatException{
 		try{
 			double d = Double.parseDouble(token);
-			Command c = (args) -> {return d;};
+			Command c = myFactory.getConstant(d);
 			if(myTokenStack.isEmpty())
 				throw new ParseFormatException("Standing alone constant: "+d);
 			myTokenStack.peek().addCommand(c);
@@ -123,7 +150,6 @@ public class SimpleParser implements Parser {
 	private void loadRules(String language) throws ParseFormatException{
 		try {
 			myLanguageRules = getLanguageRules(language);
-			myNumArgsRules = getArgumentsRules();
 			mySyntaxRules = getSyntaxRules();
 		} catch (Exception e) {
 			throw new ParseFormatException(e.getMessage());
@@ -132,15 +158,6 @@ public class SimpleParser implements Parser {
 	
 	private Map<String,String> getLanguageRules(String language) throws IOException{
 		return myLoader.getLocalizedSyntax(language);
-	}
-	
-	private Map<String,Integer> getArgumentsRules() throws IOException{
-		Map<String,Integer> argsMap = new HashMap<>();
-		Properties prop = myLoader.load("Commands");
-		prop.forEach((k,v)->{
-			argsMap.put(k.toString(), Integer.parseInt(v.toString()));
-		});
-		return argsMap;
 	}
 	
 	private Map<String,String> getSyntaxRules() throws IOException{
@@ -156,7 +173,6 @@ public class SimpleParser implements Parser {
 		SimpleParser p = new SimpleParser(null);
 		p.parse("","Chinese");
 		p.printMap(p.myLanguageRules);
-		p.printMap(p.myNumArgsRules);
 		p.printMap(p.mySyntaxRules);
 	}
 
