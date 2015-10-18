191a1d9c4e457b215b1dd9333b837d3d48db6656
Susan Lang <sml59@duke.edu>
2015-10-17 20:39:05 -0400

    Again with the CRLF issue

--- src/gui/init/Init.java
+++ src/gui/init/Init.java
@@ -5,19 +5,19 @@ import javafx.scene.Scene;
 public class Init {
 	private static int X_DIMENSION = 800;
 	private static int Y_DIMENSION = 600;
-	
+
 	public static int getXDimension(){
 		return X_DIMENSION;
 	}
-	
+
 	public static int getYDimension(){
 		return Y_DIMENSION;
 	}
 	private Scene scene;
 	private WorkspaceHandler workspaceHandler;
-	
+
 	public Init(){
-		
+
 		LanguageDialog languageDialog = new LanguageDialog();
 		workspaceHandler = new WorkspaceHandler(languageDialog.getSelected());
 		scene = new Scene(workspaceHandler.getTabPane(),X_DIMENSION,Y_DIMENSION);
