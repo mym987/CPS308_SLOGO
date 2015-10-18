a3cc8c282f0c0abc9aae14873e4d8fcd35a84b4a
Henry Yuen <Henry Yuen@henryMacbookPro>
2015-10-18 00:32:08 -0400

    Resizing Console Bar.

--- src/gui/workspace/WorkspaceHandler.java
+++ src/gui/workspace/WorkspaceHandler.java
@@ -15,6 +15,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import model.Actions;
 import model.SimpleActions;
 import model.Turtle;
@@ -85,10 +86,10 @@ public class WorkspaceHandler implements ICreateWorkspace {
 		
 		HBox navBar = createNavBar();
 		borderPane.setTop(navBar);
-		HBox bottomBar = new HBox();
-		bottomBar.getChildren().addAll(commandField, buttonFactory.createObject("enter_command"));
-		borderPane.setBottom(bottomBar);
 		
+		HBox bottomBar = createConsoleBar();
+		borderPane.setBottom(bottomBar);
+
 		Node historyView = listViewFactory.createObject("history_view");
 		
 		borderPane.setRight(historyView);
@@ -97,6 +98,15 @@ public class WorkspaceHandler implements ICreateWorkspace {
 		tabPane.getTabs().add(tab);
 		
 		WORKSPACE_NUMBER++;
+	}
+	/**
+	 * @param borderPane
+	 */
+	private HBox createConsoleBar() {
+		HBox bottomBar = new HBox();
+		HBox.setHgrow(commandField, Priority.ALWAYS);
+		bottomBar.getChildren().addAll(commandField, buttonFactory.createObject("enter_command"));
+		return bottomBar;
 	}
 	
 	// Keep this method private to prevent ButtonFactory/ColorPickerFactory from being called if createWorkspace() is not run.
---
+++ src/model/CommandHistory.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.ArrayList;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
+
+public class CommandHistory implements Observable{
+	//ObservableList<String> history = new ArrayList<String>();
+	public CommandHistory(){
+		
+	}
+	
+
+
+	@Override
+	public void addListener(InvalidationListener arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeListener(InvalidationListener arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
